23c3d2a4d7071292c62b82d8d7f5e56dd2bca6b4
Nelson Pecora <nelson@yoshokatana.com>
2017-05-17 13:23:05 -0400

    added tests for config

--- cmd/config.js
+++ cmd/config.js
@@ -1,20 +1,42 @@
-const inquirer = require('inquirer'),
-  _ = require('lodash');
+const config = require('./lib/utils/config'),
+  logger = require('./lib/utils/logger');
 
 function builder(yargs) {
   return yargs
-    .usage('Usage $0 config <alias> [value]')
+    .usage('Usage: $0 config <alias> [value]')
     .example('$0 key.local', 'View local api key')
     .example('$0 site.local localhost:3001', 'Set localhost site alias');
 }
 
 function handler(argv) {
-  console.log(argv)
+  if (argv.alias && !argv.value) {
+    // get value from .clayconfig
+    try {
+      let val = config.get(argv.alias);
+
+      if (val) {
+        logger.log(val);
+      } else {
+        logger.warn(`No value defined for "${argv.alias}"`);
+      }
+    } catch (e) {
+      logger.error(e.message);
+    }
+  } else if (argv.alias && argv.value) {
+    // set value in .clayconfig
+    try {
+      config.set(argv.alias, argv.value);
+      logger.log(`Saved ${argv.alias.split('.')[0]} ${argv.alias.split('.')[1]} = ${argv.value}`);
+    } catch (e) {
+      logger.fatal(e.message);
+    }
+  }
 }
 
 module.exports = {
-  command: 'config',
+  command: 'config <alias> [value]',
   describe: 'View or set config variables',
+  aliases: ['configure', 'cfg'],
   builder,
   handler
 };
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env node
 'use strict'; // eslint-disable-line
 const yargs = require('yargs'),
-  path = require('path'),
   logger = require('./lib/utils/logger');
 
 let argv = yargs.usage('Usage: clay <command> [options]')
   .wrap(yargs.terminalWidth())
   // commands
-  .commandDir(path.join(__dirname, 'lib', 'cmd'))
+  .commandDir('cmd')
   // common options
   .help()
   .version()
