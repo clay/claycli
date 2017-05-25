9ed45985aab2fdf510050d68b43ce4bb3369e79b
Nelson Pecora <nelson@yoshokatana.com>
2017-05-25 14:15:34 -0400

    added update notifier

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 'use strict'; // eslint-disable-line
+
 const yargs = require('yargs'),
   path = require('path'),
+  updateNotifier = require('update-notifier'),
+  pkg = require('./package.json'),
   logger = require('./lib/utils/logger'),
-  options = require('./lib/utils/shared-options');
+  options = require('./lib/utils/shared-options'),
+  notifier = updateNotifier({
+    pkg,
+    // updateCheckInterval: 1000 * 60 * 60 * 24 * 7 // 1 week
+  });
+
+if (notifier.update) {
+  notifier.notify();
+  process.exit(0);
+}
 
 let argv = yargs
   .usage('Usage: clay <command> [options]')
