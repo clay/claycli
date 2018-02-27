51f38ae3573bd4ffb89f0aac906ba43a5df72106
Nelson Pecora <nelson@yoshokatana.com>
2018-02-27 14:22:53 -0500

    convert bootstraps to dispatches

--- setup-jest.js
+++ setup-jest.js
@@ -1,5 +1,8 @@
 'use strict';
-const Highland = require('highland');
+const Highland = require('highland'),
+  mockDebug = jest.fn(),
+  mockWarn = jest.fn(),
+  mockError = jest.fn();
 
 // global version of highland that implicitly uses native promises
 // so we don't have to pass in noise in our tests
@@ -13,3 +16,8 @@ global.h = Highland.use({
 global.fetch = require('jest-fetch-mock');
 
 jest.setMock('isomorphic-fetch', fetch);
+jest.setMock('./lib/logger', () => ({
+  debug: mockDebug,
+  warn: mockWarn,
+  error: mockError
+}));
