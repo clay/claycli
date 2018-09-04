clay/claycli

commit 2ec9138eb058201939829fe893f1a0e814435ebc

allow programmatic import from stream of object dispatches (#88)

lib/cmd/import.js
@@ -97,7 +97,7 @@ function importItems(str, url, options = {}) {
 
   if (options.yaml) {
     // parse bootstraps
-    return (_.isString(str) ? h.of(str) : h(str))
+    return (h.isStream(str) ? str : h.of(str))
       .splitBy(/\n==> .*? <==\n/ig) // tail -n +1 [file1 file2 ...] | clay import
       .filter((chunk) => chunk && chunk !== '\n')
       .flatMap((str) => {
@@ -141,12 +141,14 @@ function importItems(str, url, options = {}) {
       }).parallel(concurrency);
   } else {
     // parse dispatches
-    return (_.isString(str) ? h.of(str) : h(str))
-      .split()
-      .compact()
+    return (h.isStream(str) ? str : h.of(str))
+      .flatMap((res) => {
+        // if it's a stream of strings, split them on newlines and discard any empty lines
+        return _.isString(res) ? h.of(res).split().compact() : h.of(res);
+      })
       .map((res) => {
         try {
-          return JSON.parse(res);
+          return _.isString(res) ? JSON.parse(res) : res; // allow strings or objects
         } catch (e) {
           try {
             yaml.safeLoad(str);

lib/cmd/import.test.js
@@ -50,10 +50,17 @@ describe('import', () => {
     });
   });
 
-  it('imports dispatch from stream', () => {
+  it('imports dispatch from stream of strings', () => {
     fetch.mockResponseOnce('{}');
     return lib(h.of(JSON.stringify({ '/_components/a': { b: 'c' } })), url, { key, concurrency }).collect().toPromise(Promise).then((res) => {
       expect(res).toEqual([{ type: 'success', message: 'http://domain.com/_components/a' }]);
+    });
+  });
+
+  it('imports dispatch from stream of objects', () => {
+    fetch.mockResponseOnce('{}');
+    return lib(h.of({ '/_components/obj': { b: 'c' } }), url, { key, concurrency }).collect().toPromise(Promise).then((res) => {
+      expect(res).toEqual([{ type: 'success', message: 'http://domain.com/_components/obj' }]);
     });
   });
 

commit 4f4fc0745fceeb15cb4474b49321094f4457d289

hotfix yaml handling bug introduced by #88

lib/cmd/import.js
@@ -97,7 +97,7 @@ function importItems(str, url, options = {}) {
 
   if (options.yaml) {
     // parse bootstraps
-    return (h.isStream(str) ? str : h.of(str))
+    return (_.isString(str) ? h.of(str) : h(str))
       .splitBy(/\n==> .*? <==\n/ig) // tail -n +1 [file1 file2 ...] | clay import
       .filter((chunk) => chunk && chunk !== '\n')
       .flatMap((str) => {
@@ -141,11 +141,7 @@ function importItems(str, url, options = {}) {
       }).parallel(concurrency);
   } else {
     // parse dispatches
-    return (h.isStream(str) ? str : h.of(str))
-      .flatMap((res) => {
-        // if it's a stream of strings, split them on newlines and discard any empty lines
-        return _.isString(res) ? h.of(res).split().compact() : h.of(res);
-      })
+    return (_.isString(str) ? h.of(str).split().compact() : h(str))
       .map((res) => {
         try {
           return _.isString(res) ? JSON.parse(res) : res; // allow strings or objects