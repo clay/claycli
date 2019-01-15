ec8b9c421fef8c155302fabc4291270b810c9d82
Jon Winton <winton.jon@gmail.com>
2019-01-14 23:01:20 -0500

    custom tasks first pass

--- cli/compile/custom-tasks.js
+++ cli/compile/custom-tasks.js
@@ -1,6 +1,7 @@
 'use strict';
 const pluralize = require('pluralize'),
   compile = require('../../lib/cmd/compile'),
+
   options = require('../cli-options'),
   reporter = require('../../lib/reporters'),
   helpers = require('../../lib/compilation-helpers');
@@ -13,34 +14,15 @@ function builder(yargs) {
 }
 
 function handler(argv) {
+  const t1 = Date.now(),
+    compiled = compile.customTasks({
+      watch: argv.watch,
+      minify: argv.minify,
+      inlined: argv.inlined,
+      linked: argv.linked
+    });
 
   console.log('foo')
-  // const t1 = Date.now(),
-  //   compiled = compile.fonts({
-  //     watch: argv.watch,
-  //     minify: argv.minify,
-  //     inlined: argv.inlined,
-  //     linked: argv.linked
-  //   });
-
-  // return compiled.build
-  //   .map(reporter.logAction(argv.reporter, 'fonts'))
-  //   .toArray((results) => {
-  //     const t2 = Date.now();
-
-  //     reporter.logSummary(argv.reporter, 'fonts', (successes) => {
-  //       let message = `Compiled ${pluralize('font', successes, true)} in ${helpers.time(t2, t1)}`;
-
-  //       if (compiled.watch) {
-  //         message += '\nWatching for changes...';
-  //       }
-  //       return { success: true, message };
-  //     })(results);
-
-  //     if (compiled.watch) {
-  //       compiled.watch.on('raw', helpers.debouncedWatcher);
-  //     }
-  //   });
 }
 
 module.exports = {
