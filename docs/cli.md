14f443cbbda79e76d826dddea343370d6f2dbcb6
Jon Winton <winton.jon@gmail.com>
2019-03-13 11:29:53 -0400

    updating docs

--- docs/cli.md
+++ docs/cli.md
@@ -1,6 +1,6 @@
 ---
 id: version-3.9.1-cli
-title: Claycli
+title: Clay CLI
 original_id: cli
 ---
 This is the `Clay` command line utility. Here we will provide you with documentation and examples that will help you to run all the functionalities that you need.
--- website/core/Footer.js
+++ website/core/Footer.js
@@ -24,42 +24,6 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="nav-footer" id="footer">
-        <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-                height="58"
-              />
-            )}
-          </a>
-          <div>
-            <h5>Docs</h5>
-            <a href={this.docUrl('cli.html', this.props.language)}>
-              Claycli
-            </a>
-          </div>
-          <div>
-            <h5>Community</h5>
-          </div>
-          <div>
-            <h5>More</h5>
-            <a href="https://github.com/clay/claycli">GitHub</a>
-            <a
-              className="github-button"
-              href={this.props.config.repoUrl}
-              data-icon="octicon-star"
-              data-count-href="/clay/claycli/stargazers"
-              data-show-count="true"
-              data-count-aria-label="# stargazers on GitHub"
-              aria-label="Star Claycli on GitHub">
-              Star
-            </a>
-          </div>
-        </section>
-
         <section className="copyright">{this.props.config.copyright}</section>
       </footer>
     );
