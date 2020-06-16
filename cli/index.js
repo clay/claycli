e66615ad8da03fdf0875762c6aee8dbea3cf2fd5
felkerch <chase.felker@slate.com>
2020-06-15 22:48:38 -0400

    import the command you actually use

--- cli/index.js
+++ cli/index.js
@@ -14,7 +14,15 @@ const yargs = require('yargs'),
   pkg = require('../package.json'),
   notifier = updateNotifier({
     pkg
-  });
+  }),
+  commands = {
+    cfg: 'config',
+    l: 'lint',
+    i: 'import',
+    e: 'export',
+    c: 'compile'
+  },
+  listCommands = Object.keys(commands).concat(Object.values(commands));
 
 if (notifier.update) {
   // note: this will only check for updates once per day
@@ -22,14 +30,22 @@ if (notifier.update) {
   process.exit(0);
 }
 
+let thisCommand = process.argv.filter(arg => listCommands.includes(arg))[0];
+
+if (!thisCommand) {
+  console.error('invalid command');
+  process.exit();
+}
+
+// convert alias to full command e.g. i -> import
+if (Object.keys(commands).includes(thisCommand)) {
+  thisCommand = commands[thisCommand];
+}
+
 yargs
   .usage('Usage: clay <command> [options]')
   .wrap(yargs.terminalWidth())
-  .command(require('./config'))
-  .command(require('./lint'))
-  .command(require('./import'))
-  .command(require('./export'))
-  .command(require('./compile'))
+  .command(require(`./${thisCommand}`))
   // common options
   .help()
   .version()
