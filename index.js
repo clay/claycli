ed3a298c14b5343213f9d57f6b36d90c70f79544
Daniel P. Shannon <daniel.shannon@voxmedia.com>
2021-06-30 17:21:42 -0400

    Export component mounting function.

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ module.exports.import = require('./lib/cmd/import');
 module.exports.export = require('./lib/cmd/export');
 module.exports.compile = require('./lib/cmd/compile');
 module.exports.gulp = require('gulp'); // A reference to the Gulp instance so that external tasks can reference a common package
+module.exports.mountComponentModules = require('./lib/cmd/pack/mount-component-modules').mountComponentModules;
